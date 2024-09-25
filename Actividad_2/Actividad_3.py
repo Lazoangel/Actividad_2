62283765c0b45612587b3377d035b85fe19cc7d9
Lazoangel <a.23308051281190@cbtis128.edu.mx>
2024-09-24 20:44:41 -0600

    Add files via upload

---
+++ Actividad_2/Actividad_3.py
@@ -0,0 +1,4 @@
+print ("Lazo Jimenez Angel Jesus")
+Temperatura_Fahrenheit= float (input("ingresar temperatura en Temperatura en Fahrenheit: "))
+Temperatura_Celsius= (5/9) * (Temperatura_Fahrenheit-32)
+print (f"Grados celsius {Temperatura_Celsius: .2f} grados celsius.")
